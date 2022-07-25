2db8cb9149b4b5541f37ae0e11c3a55bdacaeb87
Norberto Nicolas Velez <nnvelez@live.com>
2022-07-25 01:01:29 -0300

    Posible Solucion
    Se agregaron la teclas pedidas, disparos secuenciales con las mismas

--- DESAFIO - INGENIERIA INVERSA/Assets/Scripts/Cannon.cs
+++ DESAFIO - INGENIERIA INVERSA/Assets/Scripts/Cannon.cs
@@ -14,8 +14,21 @@ public class Cannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             Shoot();
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Shoot2();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Shoot3();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Shoot4();
         }
     }
 
@@ -29,6 +42,46 @@ public class Cannon : MonoBehaviour
             Invoke("ResetShoot", 1f);
         }
     }
+    private void Shoot2()
+    {
+        if (canShoot)
+        {
+            canShoot = false;
+            Instantiate(munition, transform);
+            Invoke("Instantiate1", 0.5f);
+            Invoke("ResetShoot", 1f);
+        }
+    }
+    private void Shoot3()
+    {
+        if (canShoot)
+        {
+            canShoot = false;
+            Instantiate(munition, transform);
+            Invoke("Instantiate1", 0.5f);
+            Invoke("Instantiate1", 1f);
+            Invoke("ResetShoot", 1f);
+        }
+    }
+    private void Shoot4()
+    {
+        if (canShoot)
+        {
+            canShoot = false;
+            Instantiate(munition, transform);
+            Invoke("Instantiate1", 0.5f);
+            Invoke("Instantiate1", 1f);
+            Invoke("Instantiate1", 1.5f);
+            Invoke("ResetShoot", 1f);
+        }
+    }
+    private void Instantiate1()
+    {
+        Instantiate(munition, transform);
+
+    }
+
+
 
     private void ResetShoot()
     {
